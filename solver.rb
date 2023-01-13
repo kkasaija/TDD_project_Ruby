2daa730d9efa4ce0ac03690bb2d401a61f4da9a4
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-13 16:03:21 +0300

    Add Solver class code block

---
+++ solver.rb
@@ -0,0 +1,7 @@
+class Solver
+  def factorial(number)
+    raise ArgumentError, 'Negative values not allowed' if number.negative?
+
+    [0, 1].include?(number) ? 1 : number * factorial(number - 1)
+  end
+end
